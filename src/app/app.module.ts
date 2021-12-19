c42192e88e010407a050f70f0b24eb0f48fac909
Natthawat Narin <natthawat.n@blockfint.com>
2021-12-19 15:27:31 +0700

    add routing page and add new assest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { TelephoneNumberComponent } from './components/telephone-number/telephone-number.component';
+import { VerifyOtpComponent } from './components/verify-otp/verify-otp.component';
+import { ButtonStyleComponent } from './components/button-style/button-style.component';
+import { InputTelephoneComponent } from './components/input-telephone/input-telephone.component';
+import { InputOtpComponent } from './components/input-otp/input-otp.component';
+import { HeaderStyleComponent } from './components/header-style/header-style.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    TelephoneNumberComponent,
+    VerifyOtpComponent,
+    ButtonStyleComponent,
+    InputTelephoneComponent,
+    InputOtpComponent,
+    HeaderStyleComponent
   ],
   imports: [
     BrowserModule,
